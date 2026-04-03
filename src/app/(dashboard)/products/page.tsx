6a99d13137643dc7149a24244ad97b069b59497e
BramaAntoro <brama4751antoro@gmail.com>
2026-04-03 09:18:49 +0700

    update redirect

---
+++ src/app/(dashboard)/products/page.tsx
@@ -0,0 +1,17 @@
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function page() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    redirect("/auth/login");
+  }
+
+  return <div>{user.id}</div>;
+}
